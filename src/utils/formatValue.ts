9ac587d998ccf221a797dbf0e95d1b35bc35e196
Anderson Viana <andiana@hotmnai.com>
2020-09-03 20:51:49 -0300

    created file in utils to format values to currency PT-BR

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,11 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (value: string | number): string => {
+  const numValue = typeof value === 'string' ? Number(value) : value;
+  const currency = new Intl.NumberFormat([], {
+    style: 'currency',
+    currency: 'BRL',
+  });
+
+  return currency.format(numValue);
+};
 
 export default formatValue;
